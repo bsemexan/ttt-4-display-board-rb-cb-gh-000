0e5c541b841d504fbef2d90a2d69da45fd3fb36e
Berry  Semexan <bsemexan@gmail.com>
2018-02-05 23:36:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,9 @@
 # out the current state.
 
 def display_board(argument)
-  puts ""
+  puts " #{argument[0]} ", " #{argument[1]}", " #{argument[2]}"
+  puts "-----------"
+  puts " #{argument[3]} ", " #{argument[4]}", " #{argument[5]}"
+  puts "-----------"
+  puts " #{argument[6]} ", " #{argument[7]}", " #{argument[8]}"
 end
